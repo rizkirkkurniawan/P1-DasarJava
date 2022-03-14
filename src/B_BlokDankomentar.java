1a72e8e8d495370690dff0bd45a4eed98c059fbe
PC 05 <rizkirkkurniawan>
2022-03-14 15:21:04 +0700

    percobaan Whitespace di Java

--- src/B_BlokDankomentar.java
+++ src/B_BlokDankomentar.java
@@ -1,4 +1,4 @@
-public class B_BlokDanKomentar {
+public class B_BlokDankomentar {
     public static void main(String[] args) {
         System.out.println("Pemrograman Berorientasi Objek dengan Java");
     }
