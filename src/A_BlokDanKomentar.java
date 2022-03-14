ac8b4669afa9674ff577ae54253bf6744a1f9839
PC 05 <rizkirkkurniawan>
2022-03-14 15:07:44 +0700

    initial project pertemuan 1 dasar Java

---
+++ src/A_BlokDanKomentar.java
@@ -0,0 +1,12 @@
+public class A_BlokDanKomentar {
+    public static void main(String[] args) {
+        System.out.println("Pemrograman Berorientasi objek dengan Java");
+        //Contoh komentar 1 baris
+        /*
+        Contoh komentar lebih dari 1 baris
+        */
+        /**
+         * Java doc --> baris ini untuk mendokumentasikan kode program
+         */
+    }
+}
