1fe250797b5ac9b5894b78171023d9de5b222f3c
PC 05 <rizkirkkurniawan>
2022-03-14 16:45:44 +0700

    mencetak nilai literal number di java

---
+++ src/G_MencetakLiteralNumber.java
@@ -0,0 +1,12 @@
+public class G_MencetakLiteralNumber {
+    public static void main(String[] args) {
+        int distance=25;
+        final long MM_TO_KM=1000000L;
+        float latitude=109.234567f;
+        final double PHI=3.14;
+        System.out.println("Nilai literal int="+distance);
+        System.out.println("Nilai literal long="+MM_TO_KM);
+        System.out.println("Nilai literal float="+latitude);
+        System.out.println("Nilai literal double="+PHI);
+    }
+}
