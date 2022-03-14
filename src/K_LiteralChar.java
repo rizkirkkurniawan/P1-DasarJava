a09c2889c56093e002acea068cfdf1cd4ce9b1b5
PC 05 <rizkirkkurniawan>
2022-03-14 17:27:02 +0700

    Menampilkan bilangan oktal ke format desimal

---
+++ src/K_LiteralChar.java
@@ -0,0 +1,15 @@
+public class K_LiteralChar {
+    public static void main(String[] args) {
+
+        char tab='\t';
+        char newline='\n';
+        char singleQuote='\'';
+        char doubleQuote='\"';
+        System.out.println("Ini contoh"+tab+"literal tab");
+        System.out.println("Ini contoh"+newline+"literal newline");
+        System.out.println(""+singleQuote+"Ini contoh"+singleQuote+"literal tab");
+        System.out.println(""+doubleQuote+"Ini contoh"+doubleQuote+"literal tab");
+
+
+    }
+}
