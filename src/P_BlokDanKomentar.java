efd4da4f34cefab81b48bcf6a243b34ee990b49e
PC 05 <rizkirkkurniawan>
2022-03-21 15:10:45 +0700

    Contoh Statement

---
+++ src/P_BlokDanKomentar.java
@@ -0,0 +1,10 @@
+public class P_BlokDanKomentar {
+    public static void main(String[] args) {
+        System.out.println("Semoga saya bisa belajar");
+        System.out.println("Pemrograman Berorientasi Dengan Java");
+        int angka = 10;
+        char charA = 'A';
+        System.out.println(angka);
+        System.out.println(charA);
+    }
+}
