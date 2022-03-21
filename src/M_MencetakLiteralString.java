4b75c22ec8e74e8c6923986e254c9dfd0cd01767
PC 05 <rizkirkkurniawan>
2022-03-21 15:29:47 +0700

    Mencetak Literal String

---
+++ src/M_MencetakLiteralString.java
@@ -0,0 +1,6 @@
+public class M_MencetakLiteralString {
+    public static void main(String[] args) {
+        String welcome = "Pemrograman Berorientasi Objek dengan Java";
+        System.out.println(welcome);
+    }
+}
