14fd8ef6c88531b03109eb991b939220a001a9b2
PC 05 <rizkirkkurniawan>
2022-03-21 15:18:12 +0700

    Membaca masukkan dari keyboard dengan dialog box

---
+++ src/R_BlokDanKomentar.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+public class R_BlokDanKomentar {
+    public static void main(String[] args) {
+    String name = JOptionPane.showInputDialog("what Is Your Name?");
+    System.out.println("My Name is"+name);
+    System.exit(0);
+}
+}
