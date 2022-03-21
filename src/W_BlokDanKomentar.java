f7c3d41ef320d94e434f9da898fbca1363e0cb97
PC 05 <rizkirkkurniawan>
2022-03-21 15:27:43 +0700

    Format Keluaran Menggunakan Fungsi Sistem.out.printf

---
+++ src/W_BlokDanKomentar.java
@@ -0,0 +1,18 @@
+public class W_BlokDanKomentar {
+    public static void main(String[] args) {
+        String nama = "Anton";
+        int umur = 21;
+        float tb = 170.4f;
+        double bb = 71.78;
+        boolean kawin = true;
+        char jk = 'P';
+        int oktal12 = 014;
+        System.out.printf("Nama : %s\n",nama);
+        System.out.printf("umur : %d\n",umur);
+        System.out.printf("tb : %f\n",tb);
+        System.out.printf("bb : %1f\n",bb);
+        System.out.printf("sudah menikah : %b\n",kawin);
+        System.out.printf("jenis kelamin : %c\n",jk);
+        System.out.printf("bilangan oktal 12 : %o\n",oktal12);
+    }
+}
