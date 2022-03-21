c342de5941166426c994b1d3ae822481b2ed7bf9
PC 05 <rizkirkkurniawan>
2022-03-21 15:22:07 +0700

    Membaca Masukkan Dari Keyboard Menggunakan Kelas Scanner

---
+++ src/T_BlokDanKomentar.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class T_BlokDanKomentar {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is your name? :");
+        String name = input.nextLine();
+        System.out.printf("My name is " +name);
+    }
+
+}
